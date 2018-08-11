19f6e5683e90410f6552919717547a0dfe5868a3
Supachai Keemthong <k.supachai@Supachais-MacBook-Pro.local>
2018-08-12 01:41:13 +0700

    Create app.js file

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const os = require('os');
+
+// var z = (a, b) => {
+//     return a + b;
+// }
+
+// var x = z(5, 6);
+
+// console.log(x);
+
+// console.log('Start');
+
