b9ed4cfb8575737e98dddb67c12ea1fb55280de2
Supachai Keemthong <k.supachai@Supachais-MacBook-Pro.local>
2018-08-12 02:18:51 +0700

    test1

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+console.log('Start app.js');
+
 const fs = require('fs');
 const os = require('os');
+const notes = require('./note.js');
 
-// var z = (a, b) => {
-//     return a + b;
-// }
+fs.appendFile('message.txt', `data is : ${notes.testNumber} and ${notes.calc(5, 6, 7)}` , (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+  });
+
+
+
+
+
+var z = (a, b) => {
+    return a + b;
+}
 
 // var x = z(5, 6);
 
